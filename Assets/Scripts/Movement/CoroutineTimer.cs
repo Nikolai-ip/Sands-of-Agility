a725db4c940273b59c51a9804a1eb204439f6c6e
Artem <artemoshepkov790@gmail.com>
2024-07-25 00:14:51 +0700

    Add chain

---
+++ Assets/Scripts/Movement/CoroutineTimer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Movement
+{
+    public class CoroutineTimer : MonoBehaviour
+    {
+        private Coroutine _coroutine;
+        private WaitForSeconds _waitForSec;
+        private Action _onTimeStart;
+        private Action _onTimeStopped;
+
+        [SerializeField] private float _time = 1f;
+
+        public void Init(Action onTimeStart, Action onTimeStopped)
+        {
+            _onTimeStart = onTimeStart;
+            _onTimeStopped = onTimeStopped;
+        }
+        
+        private void Awake()
+        {
+            _waitForSec = new WaitForSeconds(_time);
+        }
+        
+        private void OnValidate()
+        {
+            _waitForSec = new WaitForSeconds(_time);
+        }
+
+        public void StartTimer()
+        {
+            _onTimeStart();
+
+            _coroutine = StartCoroutine(ChangeCoyoteJumpAfterTimer());
+        }
+
+        public void StopTimer()
+        {
+            if (_coroutine == null)
+                return;
+            
+            StopCoroutine(_coroutine);
+            _onTimeStopped();
+        }
+
+        private IEnumerator ChangeCoyoteJumpAfterTimer()
+        {
+            yield return _waitForSec;
+            _onTimeStopped();
+        }
+    }
+}
--- Assets/Scripts/Movement/JumpBehaviour.cs
+++ Assets/Scripts/Movement/JumpBehaviour.cs
@@ -6,36 +6,65 @@ namespace Movement
     public class JumpBehaviour : MonoBehaviour
     {
         private Rigidbody2D _rb;
-        private bool _isOnGround;
-        private float _jumpForce;
+        private bool _isOnGround = false;
+        private bool _isCoyoteJump = false;
+        private bool _isJumped = false;
+        [SerializeField] private float _jumpForce;
 
+        [SerializeField] private CoroutineTimer _timer;
         [SerializeField] private GroundChecker _groundChecker;
-
-        public float _metersToJump;
+        [SerializeField] private float _metersToJump;
         
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
+
+            _timer.Init(() => { _isCoyoteJump = true; }, () => { _isCoyoteJump = false; });
+            
             _groundChecker.OnLayerStateChanged += UpdateIsOnGround;
+            _groundChecker.OnLayerEntered += _timer.StopTimer;
+            _groundChecker.OnLayerExited += _timer.StartTimer;
         }
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space) && _groundChecker.IsLayerStaying) 
+            if (Input.GetKeyDown(KeyCode.Space) && CanJump())
+            {
                 Jump();
+                _isJumped = true;
+            }
         }
 
-        private void OnDisable() => _groundChecker.OnLayerStateChanged += UpdateIsOnGround;
+        private void OnDisable()
+        {
+            _groundChecker.OnLayerStateChanged -= UpdateIsOnGround;
+            _groundChecker.OnLayerEntered -= _timer.StopTimer;
+            _groundChecker.OnLayerExited -= _timer.StartTimer;
+        }
 
-        private void OnValidate() => UpdateJumpForce();
+        private void OnValidate()
+        {
+            // UpdateJumpForce();
+        }
 
         private void Jump()
         {
-            if (_isOnGround)
-                _rb.AddForce(_jumpForce * Vector2.up);
+            var jumpForce = _jumpForce;
+            if (_rb.velocity.y < 0)
+                jumpForce -= _rb.velocity.y;
+            
+            _rb.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
         }
-        
-        private void UpdateIsOnGround() => _isOnGround = _groundChecker.IsLayerStaying;
+
+        private bool CanJump() => !_isJumped && (_isOnGround || _isCoyoteJump);
+
+        private void UpdateIsOnGround()
+        {
+            _isOnGround = _groundChecker.IsLayerStaying;
+            if (_isOnGround)
+                _isJumped = false;
+        }
+
         private void UpdateJumpForce() => _jumpForce = _metersToJump * Config.JumpForceToMeters;
     }
 }
